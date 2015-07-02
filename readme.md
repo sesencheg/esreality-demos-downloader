8069eb38d75afd192956888156c4712da594aff9
Danmer <danmer.ekb@gmail.com>
2015-07-03 00:31:10 +0500

    Init commit

---
+++ readme.md
@@ -0,0 +1,49 @@
+# Cyberfight Demos Downloader
+
+>  The script for downloading demos from the [cyberfight.ru](http://cyberfight.ru)
+
+## Install
+
+Required: [node.js](https://nodejs.org/) > 0.12 or [io.js](https://iojs.org) > 1.0
+
+For Windows users also required: [Python 2.7](http://www.python.org/download/) and [Visual Studio Express 2013 for Windows Desktop](http://www.visualstudio.com/downloads/download-visual-studio-vs#d-express-windows-desktop). See [jsdom docs](https://github.com/tmpvar/jsdom/tree/3.x#windows) for details.
+
+```
+git clone https://github.com/danmer/c58-demos-downloader.git
+cd c58-demos-downloader
+npm install
+```
+
+## Usage
+
+```
+npm start [game_id1] [game_id2] ...
+```
+
+### Game Ids
+
+| id | game |
+| --:| :--- |
+|  5 | Counter Strike |
+| 26 | Counter-Strike: Global Offensive |
+| 19 | DOOM 3 |
+| 28 | DOTA 2 |
+| 23 | FIFA |
+| 24 | Need For Speed |
+| 21 | Painkiller |
+| 16 | Quake |
+| 13 | Quake 2 |
+|  1 | Quake 3 |
+| 22 | Quake 4 |
+| 25 | Quake Live |
+| 15 | Return to Castle Wolfenstein |
+|  9 | Starcraft Brood War |
+| 27 | Starcraft 2 |
+| 10 | Unreal Tournament |
+| 18 | Unreal Tournament 2004 |
+| 17 | Unreal Tournament 2003 |
+| 14 | War Craft 3 |
+
+## License
+
+MIT © Egor Kotlyarov
---
+++ server.js
@@ -0,0 +1,184 @@
+/**
+ * Cyberfight Demos Downloader 0.1.0
+ * Copyright 2015 Egor Kotlyarov aka Danmer
+ * Available under MIT license
+ *
+ * Usage: npm start [game_id1] [game_id2] ...
+ */
+
+var fs      = require('fs');
+var _       = require('lodash');
+var request = require('request');
+var jsdom   = require('jsdom');
+var mkdirp  = require('mkdirp');
+
+var gameIds = process.argv.splice(2);
+
+parseGameIds(gameIds).then(function() {
+  console.log('done!')
+})
+
+function parseGameIds(gamesIds) {
+  console.log('parse games ids. count: ', gamesIds.length);
+  return new Promise(function(resolve, reject) {
+    var gameId;
+    var index = 0;
+    var gamesIdsCount = gamesIds.length;
+    nextGameId();
+    function nextGameId() {
+      gameId = gamesIds.shift();
+      if (gameId) {
+        index++;
+        console.log('parse game', gameId, '(' + index + '/' + gamesIdsCount + ')');
+        getPagesLinks(gameId).then(parsePagesLinks).then(nextGameId);
+      } else {
+        resolve();
+      }
+    }
+  });
+}
+
+function getPagesLinks(gameId) {
+  console.log('request pages links');
+  return new Promise(function(resolve, reject) {
+    jsdom.env({
+      url: 'http://cyberfight.ru/site/demos/search/?game_id=' + gameId + '&order_by=time_posted&order_type=desc&qry=&page=0',
+      done: function(errors, window) {
+        var pagesLinks = [];
+        if (!errors) {
+          var $pageLinks = window.document.querySelectorAll('a[href^="/site/demos/search/?game_id=' + gameId + '&order_by=time_posted&order_type=desc&qry=&page="]');
+          pagesLinks = _.uniq(_.pluck($pageLinks, 'href'));
+        } else {
+          console.error('request error', errors);
+        }
+        // get 1 random page link for test
+        // pagesLinks = _.sample(pagesLinks, 1);
+        window.close();
+        resolve(pagesLinks);
+      }
+    });
+  });
+}
+
+function parsePagesLinks(pagesLinks) {
+  console.log('parse page links');
+  return new Promise(function(resolve, reject) {
+    var pageLink = '';
+    var index = 0;
+    var pagesLinksCount = pagesLinks.length;
+    nextPageLink();
+    function nextPageLink() {
+      pageLink = pagesLinks.shift();
+      if (pageLink) {
+        index++;
+        console.log('parse page link', pageLink, '(' + index + '/' + pagesLinksCount + ')');
+        getDemosLinks(pageLink).then(parseDemosLinks).then(nextPageLink);
+      } else {
+        resolve();
+      }
+    }
+  });
+}
+
+function getDemosLinks(pageLink) {
+  console.log('request demos links on pageLink');
+  return new Promise(function(resolve, reject) {
+    jsdom.env({
+      url: pageLink,
+      done: function(errors, window) {
+        var demosLinks = [];
+        if (!errors) {
+          var $demosLinks = window.document.querySelectorAll('tr[valign="middle"] a[href^="/site/demos/"][href$="/"]');
+          demosLinks = _.uniq(_.pluck($demosLinks, 'href'));
+        } else {
+          console.error('request error', errors);
+        }
+        // get 3 random demo links for test
+        // demosLinks = _.sample(demosLinks, 3);
+        window.close();
+        resolve(demosLinks);
+      }
+    });
+  });
+}
+
+function parseDemosLinks(demosLinks) {
+  console.log('parse demos links. count:', demosLinks.length);
+  return new Promise(function(resolve, reject) {
+    var demoLink;
+    var index = 0;
+    var demosLinksCount = demosLinks.length;
+    nextDemoLink();
+    function nextDemoLink() {
+      demoLink = demosLinks.shift();
+      if (demoLink) {
+        index++;
+        console.log('parse demo link', demoLink, '(' + index + '/' + demosLinksCount + ')');
+        parseDemoLink(demoLink).then(nextDemoLink);
+      } else {
+        resolve();
+      }
+    }
+  });
+}
+
+function parseDemoLink(demoLink) {
+  return new Promise(function(resolve, reject) {
+    var demoDir = 'demos/' + demoLink.match(/\d+/)[0];
+    fs.exists(demoDir, function(exists) {
+      if (!exists) {
+        getDemoInfo(demoLink).then(downloadDemo).then(resolve);
+      } else {
+        resolve()
+      }
+    });
+  });
+}
+
+function getDemoInfo(demoLink) {
+  console.log('request demo info');
+  return new Promise(function(resolve, reject) {
+    jsdom.env({
+      url: demoLink,
+      done: function(errors, window) {
+        var demoInfo = null
+        if (!errors) {
+          var $infoTable = window.document.querySelectorAll('.blockheaddarkBig')[0].parentNode.parentNode;
+          demoInfo = {
+            source: demoLink,
+            id: demoLink.match(/\d+/)[0],
+            name: $infoTable.querySelector('tr:nth-child(1)').textContent.trim(),
+            pov: $infoTable.querySelector('tr:nth-child(2) td:nth-child(2)').textContent.trim(),
+            map: $infoTable.querySelector('tr:nth-child(3) td:nth-child(2)').textContent.trim(),
+            type: $infoTable.querySelector('tr:nth-child(4) td:nth-child(2)').textContent.trim(),
+            tourney: $infoTable.querySelector('tr:nth-child(5) td:nth-child(2)').textContent.trim(),
+            downloads: $infoTable.querySelector('tr:nth-child(6) td:nth-child(2)').textContent.trim(),
+            description: $infoTable.querySelector('tr:nth-child(7) td:nth-child(2)').textContent.trim(),
+            fileName: $infoTable.querySelector('tr:nth-child(8)').textContent.trim().split('\n')[0].trim(),
+            fileSize: $infoTable.querySelector('tr:nth-child(8)').textContent.trim().split('\n')[2].trim(),
+            fileLink: $infoTable.querySelector('tr:nth-child(8) a').href
+          };
+          window.close();
+        } else {
+          console.error('request error', errors);
+        }
+        resolve(demoInfo);
+      }
+    });
+  });
+}
+
+function downloadDemo(demoInfo) {
+  console.log('download demo');
+  return new Promise(function(resolve, reject) {
+    var demoDir = 'demos/' + demoInfo.id;
+    fs.exists(demoDir, function(exists) {
+      if (!exists) {
+        mkdirp(demoDir, function() {
+          fs.writeFile(demoDir + '/info.json', JSON.stringify(demoInfo));
+          request(demoInfo.fileLink, resolve).pipe(fs.createWriteStream(demoDir + '/' + demoInfo.fileName))
+        })
+      }
+    });
+  });
+}
